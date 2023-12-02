3428847cc30b01492672992ba5ca0fd2caeea444
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-01 20:35:05 -0600

    add: watch mode

--- bases/app.ts
+++ bases/app.ts
@@ -1,2 +1,10 @@
-const msg: string = 'Hola mundo'
-console.log(msg)
+const msg: string = 'Hola mundo!';
+
+const hero = {
+  name: 'Spider-man',
+  age: 22
+}
+
+hero.age = 50
+
+console.log(hero.age + 1)
