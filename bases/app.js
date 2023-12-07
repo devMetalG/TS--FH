44985fa589bfdca7313dc25c57cecaf5b898cb5f
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-06 23:57:09 -0600

    add: numbers

--- bases/app.js
+++ bases/app.js
@@ -2,5 +2,7 @@
 (() => {
     const a = 10;
     let b = 10;
-    console.log(a);
+    // console.log(a)
+    const body = document.querySelector('body');
+    body.style.background = '#111';
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,5 +1,8 @@
 (()=>{
   const a:number = 10;
   let b:number = 10;
-  console.log(a)
+  // console.log(a)
+
+  const body:any = document.querySelector('body');
+  body.style.background = '#111';
 })()
