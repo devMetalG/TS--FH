18226831fd5bcb73eb91590da604f640b2757e0e
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-07 22:52:34 -0600

    add: tarea1

--- bases/arrays.ts
+++ bases/arrays.ts
@@ -2,6 +2,6 @@
   // const numbers:(string | number)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const numbers:number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  const villians = ['Duende Verde', 'Dormamu', 'Omega Rojo'];
+  const villians:string[] = ['Duende Verde', 'Dormamu', 'Omega Rojo'];
   villians.forEach(villian => console.log(villian.toUpperCase()));
 })()
