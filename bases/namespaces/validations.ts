6b09c8df273f316ccf3c590e8a2e9ebf5a4af971
Guillermo Franco <fra_borjon@hotmail.com>
2025-03-13 23:54:08 -0600

    nameSpaces TypeScript

---
+++ bases/namespaces/validations.ts
@@ -0,0 +1,9 @@
+namespace validations {
+  export const validateText = (text:string): boolean => {
+    return (text.length > 3) ? true: false;
+  }
+
+  export const validateDate = (date: Date): boolean => {
+    return (isNaN(date.valueOf())) ? false : true;
+  }
+}
